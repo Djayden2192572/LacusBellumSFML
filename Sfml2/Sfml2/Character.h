13e297929988062f47ad388f96071af9b71d614f
Inflol13 <Jackvv12@outlook.com>
2025-09-11 11:25:29 +0200

    test

---
+++ Sfml2/Sfml2/Character.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+class Character
+{
+public:
+		
+		Character();
+		~Character();
+
+		void MoveTo(float x, float y);
+		void Update(float dt);
+private:
+		int health;
+		int mana;
+		std::string name;
+
+protected:
+};
+
